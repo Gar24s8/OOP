3758b28fe0645ee4cb43cc851fc0f0bad6606331
gar24s8 <gar24s8@gmail.com>
2023-03-01 09:35:49 +0300

    First oop

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -2,12 +2,15 @@ import testobject.Computer;
 
 public class MyProgram {
     public static void main(String[] args) {
-        Computer comp = new Computer();
-        comp.name = "IBM";
-        comp.hdd=350;
+        Computer comp = new Computer("IBM", 2048, 350, 2);
+
+//        comp.setName("IBM");
+//        comp.setRam(2048);
+//        comp.setHdd(350);
         comp.on();
         comp.load();
         comp.off();
+
     }
 }
 
--- src/testobject/Computer.java
+++ src/testobject/Computer.java
@@ -1,20 +1,69 @@
 package testobject;
 
 public class Computer {
-    public String name;
-    public int ram;
-    public int hdd;
-    public double weight;
+    private String name;
+    private int ram;
+    private int hdd;
+    private double weight;
+
+    public Computer(String name, int ram, int hdd, int weight){
+        this.name = name;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.weight = weight;
+    }
 
     public void on() {
-        System.out.println("я включился "+name);
+        print("я включился " + name);
     }
-
     public void off() {
-        System.out.println("я выключился");
+        print("я выключился");
+    }
+    public void load() {
+        print("я загружаюсь, мой объем жесткого диска равен " + hdd + " Гб");
+    }
+    private void print(String str){
+        System.out.println(str);
     }
 
-    public void load() {
-        System.out.println("я загружаюсь, мой объем жесткого диска равен "+hdd+" Гб");
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int newRam) {
+        if (newRam > 0) {
+            ram = newRam;
+        } else {
+            System.out.println("Переданное значение " + newRam + " не может быть отрицательным");
+        }
+
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double newWeight) {
+        weight = newWeight;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int newHdd) {
+        if (newHdd > 0) {
+            hdd = newHdd;
+        } else {
+            System.out.println("Переданное значение " + newHdd + " не может быть отрицательным");
+        }
     }
 }

e7a633ae99a2f5199095a2d1a2f35ce4ba5fb3a9
gar24s8 <gar24s8@gmail.com>
2023-03-01 10:06:17 +0300

    Vehicle

--- src/BookOOP/Vehicle.java
+++ src/BookOOP/Vehicle.java
@@ -1,2 +1,32 @@
-package BookOOP;public class Vehicle {
+package BookOOP;
+
+public class Vehicle {
+    int passengers; //количество пассажиров
+    int fuelcap; //емкость топливного бака
+    int mpg; // средний расход топлива
+}
+
+// в этом классе объявляется объект типа Vehicle
+class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportcar = new Vehicle();
+        int range1, range2;
+
+        // присваиваем значение полям в объекте minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        // присваиваем значение полям в объекте sportcar
+        sportcar.passengers = 2;
+        sportcar.fuelcap = 14;
+        sportcar.mpg = 12;
+
+        // Расчет дальности поездки с полным баком горючего
+        range1 = minivan.fuelcap * minivan.mpg;
+        range2 = sportcar.fuelcap* sportcar.mpg;
+        System.out.println("Минифургон может привезти " + minivan.passengers + " пассажиров на расстояние " + range1 + " миль");
+        System.out.println("Спортивный автомобиль может привезти " + sportcar.passengers + " пассажиров на расстояние " + range2 + " миль");
+    }
 }
